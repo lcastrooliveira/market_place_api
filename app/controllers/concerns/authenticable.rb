e4fab1b653eef16c49c01c4a3c81e064c8b50c57
Lucas Oliveira <lcastro.oliveira@gmail.com>
2016-11-27 11:14:58 -0200

    Adds user_signed_in? method to know whether the user is logged in or not

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -6,4 +6,8 @@ module Authenticable
   def authenticate_with_token!
     render json: { errors: 'Not authenticated' }, status: :unauthorized unless current_user.present?
   end
+
+  def user_signed_in?
+    current_user.present?
+  end
 end

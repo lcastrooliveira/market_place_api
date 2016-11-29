0a8382eb8d0f911caeb91e7c5ae3286a9c8492e6
Lucas Oliveira <lcastro.oliveira@gmail.com>
2016-11-29 15:56:15 -0200

    Adds search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ActiveRecord::Base
   belongs_to :user
   validates :title, :user, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
+
+  scope :filter_by_title, ->(keyword) { where('lower(title) LIKE ?', "%#{keyword.downcase}%") }
+  scope :above_or_equal_to_price, ->(price) { where('price >= ?', price) }
+  scope :below_or_equal_to_price, ->(price) { where('price <= ?', price) }
+  scope :recent, -> { order(:updated_at) }
 end

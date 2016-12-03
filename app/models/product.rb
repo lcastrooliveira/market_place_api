fa1a4899f6537bf3604eafa60f9fbda433f5928b
Lucas Oliveira <lcastro.oliveira@gmail.com>
2016-12-03 16:54:46 -0200

    Associates products and orders with a placements model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
   belongs_to :user
   validates :title, :user, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
+  has_many :placements
+  has_many :orders, through: :placements
 
   scope :filter_by_title, ->(keyword) { where('lower(title) LIKE ?', "%#{keyword.downcase}%") }
   scope :above_or_equal_to_price, ->(price) { where('price >= ?', price) }

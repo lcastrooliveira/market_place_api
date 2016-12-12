45bf4c62e745c5e5385b074b072ca11e38a6e6d5
Lucas Oliveira <lcastro.oliveira@gmail.com>
2016-12-12 11:26:12 -0200

    Allows the order to be placed along with product quantity

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,11 @@ class Order < ActiveRecord::Base
   def set_total!
     self.total = products.map(&:price).inject(:+)
   end
+
+  def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      id, quantity = product_id_and_quantity
+      self.placements.build(product_id: id)
+    end
+  end
 end
